c1977b5b21a776adfaf43cb021eb3eaf31964453
Lewis <Lewiswilson1497@gmail.com>
2021-01-26 22:15:42 +0000

    Added firestore export that can be used through the program

--- components/Firebase/firebase.js
+++ components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import * as firebase from 'firebase';
 import 'firebase/auth';
+import 'firebase/firestore'
 
 import firebaseConfig from './firebaseConfig';
 
@@ -8,6 +9,8 @@ import firebaseConfig from './firebaseConfig';
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
+
+export const firebase_db = firebase.firestore();
 
 export const auth = firebase.auth();
 
