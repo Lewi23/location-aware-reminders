b6c6b1302dc6175e20d84f18eea6c41513fef491
Lewis <Lewiswilson1497@gmail.com>
2021-01-27 16:50:50 +0000

    Pulling data from firebase when user edits database

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import React, { useState, useEffect  } from 'react';
+//import {Picker} from '@react-native-picker/picker';
+import { View, StyleSheet, Button, FlatList, Text } from 'react-native';
 
 import AppButton from '../components/AppButton';
 import useStatusBar from '../hooks/useStatusBar';
 import { logout } from '../components/Firebase/firebase';
+import { firebase_db } from '../components/Firebase/firebase'
+import { auth } from '../components/Firebase/firebase'
+import { out } from 'react-native/Libraries/Animated/src/Easing';
+
+
+
+
+
+
 
 export default function HomeScreen({ navigation }) {
+
+
   useStatusBar('dark-content');
+
+  const [ loading, setLoading ] = useState(true);
+  const [ reminders, setReminders ] = useState([]);
+
+ 
+  function onResult(QuerySnapshot) {
+    QuerySnapshot.forEach(doc => {
+      const { location, reminder } = doc.data();
+      console.log(location + " " + reminder)
+      
+    });
+  }
+  
+  function onError(error) {
+    console.error(error);
+  }
+  
+  firebase_db
+    .collection(auth.currentUser.uid)
+    .onSnapshot(onResult, onError);
 
   async function handleSignOut() {
     try {
@@ -16,10 +48,36 @@ export default function HomeScreen({ navigation }) {
     }
   }
 
+
+  const renderItem = ({ item }) => (
+    <Item title={item.reminder} />
+  )
+
+  const Item = ({ title }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+  
+
+
   return (
     <View style={styles.container}>
       <Button title="Sign Out" onPress={handleSignOut} />
       <AppButton title="Create Event" color="grey" onPress={() => navigation.navigate('AddEvent')}/>
+      {/* <FlatList 
+        style={{flex: 1}}
+        data={reminders}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <reminders {...item} />}
+      /> */}
+      
+      {/* <FlatList
+        data={reminders}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      /> */}
+
     </View>
 
 
