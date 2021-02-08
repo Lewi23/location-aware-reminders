153b2d0903935c22b18d07349a9346f484c65fbd
Lewis <Lewiswilson1497@gmail.com>
2021-02-08 23:08:41 +0000

    Added completed reminders page to navigation

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -3,14 +3,20 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from '../screens/HomeScreen';
 import AddEventScreen from '../screens/AddEventScreen';
+import completedReminders from '../screens/CompletedReminders';
+
 
 const Stack = createStackNavigator();
 
+
+
 export default function AppStack() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="AddEvent" component={AddEventScreen} />
-    </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Reminders" component={HomeScreen} />
+        <Stack.Screen name="AddEvent" component={AddEventScreen} />
+        <Stack.Screen name="completedReminders" component={completedReminders}/>
+      </Stack.Navigator>
+
   );
 }
