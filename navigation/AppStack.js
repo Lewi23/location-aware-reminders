fb48b8b20b3b51503217357d53ceedd8fb1f8bf9
Lewis <Lewiswilson1497@gmail.com>
2021-02-12 17:26:02 +0000

    Added styling example to the stack navigator

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -12,7 +12,15 @@ const Stack = createStackNavigator();
 
 export default function AppStack() {
   return (
-      <Stack.Navigator>
+      <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#9AC4F8",
+        },
+        headerTintColor: "white",
+        headerBackTitle: "Back",
+      }}
+    >
         <Stack.Screen name="Reminders" component={HomeScreen} />
         <Stack.Screen name="AddEvent" component={AddEventScreen} />
         <Stack.Screen name="completedReminders" component={completedReminders}/>
