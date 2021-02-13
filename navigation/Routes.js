e3c151322bff3ad3cda977c311e47b4def82a277
Lewis <Lewiswilson1497@gmail.com>
2021-02-13 19:19:37 +0000

    Tab + Stack navigation working together

--- navigation/Routes.js
+++ navigation/Routes.js
@@ -1,12 +1,33 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { auth } from '../components/Firebase/firebase';
+
+
 import navigationTheme from './navigationTheme';
+
+
 import AuthStack from './AuthStack';
-import AppStack from './AppStack';
+// import AppStack from './AppStack';
+
+
+
 import { AuthUserContext } from './AuthUserProvider';
 import Spinner from '../components/Spinner';
+
+
+
+// Screens
+import AddEventScreen from '../screens/AddEventScreen';
+import completedRemindersScreen from '../screens/CompletedReminders';
+import HomeScreen from '../screens/HomeScreen';
+
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
 
 export default function Routes() {
   const { user, setUser } = useContext(AuthUserContext);
@@ -31,9 +52,44 @@ export default function Routes() {
     return <Spinner />;
   }
 
+  if (user){
+
+  } else {
+
+  }
+
   return (
-    <NavigationContainer theme={navigationTheme}>
-      {user ? <AppStack /> : <AuthStack />}
+    <NavigationContainer>
+      {/* {user ? <AppStack /> : <AuthStack />} */}
+     {/* <AppStack />  */}
+
+     <Stack.Navigator>
+        <Stack.Screen name="Tabs" component={TabNavigation}/> 
+        <Stack.Screen name="Reminders" component={HomeScreen} />
+        <Stack.Screen name="AddEvent" component={AddEventScreen} />
+    </Stack.Navigator>
+
     </NavigationContainer>
   );
 }
+
+
+function AppStack() {
+  return (
+      <Stack.Navigator>
+        <Stack.Screen name="Tabs" component={MainTabNavigator}/> 
+        <Stack.Screen name="Reminders" component={HomeScreen} />
+        <Stack.Screen name="AddEvent" component={AddEventScreen} />
+      </Stack.Navigator>
+  );
+};
+
+function TabNavigation() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Reminders" component={HomeScreen} />
+      <Tab.Screen name="completedReminders" component={completedRemindersScreen}/>
+    </Tab.Navigator>
+  )
+}
+
