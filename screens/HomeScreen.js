7aa1e3a93e338f9240e889ddea56b6a4fca286cc
Lewis <Lewiswilson1497@gmail.com>
2021-02-15 23:05:11 +0000

    Working POI alerts using the ontology

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,66 @@
 import React, { useState, useEffect  } from 'react';
-import { View, StyleSheet, Button, FlatList, Text, ListItem } from 'react-native';
+import { View, StyleSheet, Button, FlatList, Text, TouchableOpacity, TouchableHighlight } from 'react-native';
+import * as Location from 'expo-location';
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
+import Modal from 'react-native-modal';
+import MapView from 'react-native-maps';
+
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+
+import completedReminders from './CompletedReminders';
+
 
 import AppButton from '../components/AppButton';
 import useStatusBar from '../hooks/useStatusBar';
 import { logout } from '../components/Firebase/firebase';
 import { firebase_db } from '../components/Firebase/firebase'
 import { auth } from '../components/Firebase/firebase'
+import colors from '../utils/colors';
+import { AppOwnership } from 'expo-constants';
+
+
+const Item = ({ item, onPress, style }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
+    <Text style={styles.title}>{item.reminder + "\n" + item.location + "\n" + item.completed}</Text>
+  </TouchableOpacity>
+);
+
+
 
 
 export default function HomeScreen({ navigation }) {
   useStatusBar('light-content');
 
-  const [ reminders, setReminders] = useState();
-  const [ nearbyPOIs, setNearbyPOIs] = useState();
+  // Reminders a user has created 
+  const [ reminders, setReminders ] = useState();
+  // Current locations we are looking out for based on the reminders created 
+  const [ reminderLocations, setReminderLocations ] = useState();
+  // Nearby POIs we have found based on reminders locations we are intrested in
+  const [ nearbyPOIs, setNearbyPOIs ] = useState();
+  const [ selectedId, setSelectedId ] = useState(null);
 
+  //console.log(selectedId);
+
+  const renderItem = ({ item }) => {
+
+    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        style={{ backgroundColor }}
+      
+      />
+    );
+  };
+
+
+
+  
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestPermissionsAsync();
@@ -28,27 +73,52 @@ export default function HomeScreen({ navigation }) {
       
       let result = await fetch('https://0186u6yf60.execute-api.eu-west-2.amazonaws.com/v1/check_location?lon=' + location["coords"]['longitude'] + '&lat=' + location["coords"]['latitude']);
       let POIs = await result.json();
-      setNearbyPOIs(POIs);
 
+      const found_pois = [];
 
-    })();
-  }, []);
-  useEffect(() => {
-    return firebase_db.collection(auth.currentUser.uid).onSnapshot(querySnapshot => {
-      const list = [];
-      querySnapshot.forEach(doc => {
-        const { location, reminder, completed } = doc.data();
-        if(list.completed == false){
-          list.push({
-            id: doc.id,
-            location,
-            reminder,
-            completed,
-          });
+      POIs['body'].forEach(function(Element) {
+        if(reminderLocations.includes(Element[2])){
+          //console.log(Element);
+          found_pois.push(Element);
+          alert(Element);
         }
       });
 
-      setReminders(list);
+
+      // set state here (Object we used to build our map)
+      setNearbyPOIs(found_pois);
+      console.log(nearbyPOIs);
+
+    })();
+  }, []);
+
+  
+
+  // Populates the reminders state
+  useEffect(() => {
+    return firebase_db.collection(auth.currentUser.uid).onSnapshot(querySnapshot => {
+      const reminder_list = [];
+      const locations = [];
+      querySnapshot.forEach(doc => {
+        const { location, reminder, completed } = doc.data();
+        if(!completed){
+          reminder_list.push({
+            id: doc.id,
+            reminder,
+            completed,
+          });
+
+          locations.push(location);
+        }
+      });
+
+      // Tracking users reminders 
+      setReminders(reminder_list);
+      //console.log(reminders)
+      // Tracking locations of reminders
+      setReminderLocations(locations);
+      //console.log(locations);
+    
 
     });
   }, []);
@@ -61,37 +131,60 @@ export default function HomeScreen({ navigation }) {
     }
   }
 
+
+
+
+
+
+  
+
+
   return (
+  
+
+    
     <View style={styles.container}>
-      <Button title="Sign Out" onPress={handleSignOut} />
-      <AppButton title="Create Event" color="black" onPress={() => navigation.navigate('AddEvent')}/>
+
+   
+      
+      {/* <Button title="Sign Out" onPress={handleSignOut} /> */}
+     
 
       <FlatList
-            data={reminders}
-            renderItem={({ item }) => {
-              return <Text style={styles.item}>{item.reminder}</Text>;
-            }}
-            keyExtractor={item => item.id}
+        data={reminders}
+        // renderItem={({ item }) => {
+        //   return <Text style={styles.item}>{item.reminder + "\n" + item.location + "\n" + item.completed}</Text>;
+        // }}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        extraData={selectedId}
       />
 
         <ActionButton
-          size={80}
+          size={70}
           buttonColor="rgb(60, 179, 113)"
           onPress={() => { navigation.navigate('AddEvent')}}
         />
-            
+
+
+  
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  button_container:{
     flex: 1
   },
   item: {
-    backgroundColor: '#f9c2ff',
+    backgroundColor: 'black',
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
+    fontSize: 20,
+    color: 'white'
   },
 });
