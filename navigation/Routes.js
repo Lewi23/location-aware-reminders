acaacf662ac947bcbdffd1070e71ef4befd16544
Lewis <Lewiswilson1497@gmail.com>
2021-03-04 21:54:11 +0000

    Fixed formatting and updated styling

--- navigation/Routes.js
+++ navigation/Routes.js
@@ -1,22 +1,22 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useContext, useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import { auth } from '../components/Firebase/firebase';
-import { AuthUserContext } from './AuthUserProvider';
-import Spinner from '../components/Spinner';
+import { auth } from "../components/Firebase/firebase";
+import { AuthUserContext } from "./AuthUserProvider";
+import Spinner from "../components/Spinner";
 
 // Post-auth screens
-import AddEventScreen from '../screens/AddEventScreen';
-import completedRemindersScreen from '../screens/CompletedReminders';
-import HomeScreen from '../screens/HomeScreen';
+import AddEventScreen from "../screens/AddEventScreen";
+import completedRemindersScreen from "../screens/CompletedReminders";
+import HomeScreen from "../screens/HomeScreen";
 
 // Pre-auth screens
-import WelcomeScreen from '../screens/WelcomeScreen';
-import RegisterScreen from '../screens/RegisterScreen';
-import LoginScreen from '../screens/LoginScreen';
-import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
+import WelcomeScreen from "../screens/WelcomeScreen";
+import RegisterScreen from "../screens/RegisterScreen";
+import LoginScreen from "../screens/LoginScreen";
+import ForgotPasswordScreen from "../screens/ForgotPasswordScreen";
 
 // Build navigators
 const Stack = createStackNavigator();
@@ -28,7 +28,7 @@ export default function Routes() {
 
   useEffect(() => {
     // onAuthStateChanged returns an unsubscriber
-    const unsubscribeAuth = auth.onAuthStateChanged(async authUser => {
+    const unsubscribeAuth = auth.onAuthStateChanged(async (authUser) => {
       try {
         await (authUser ? setUser(authUser) : setUser(null));
         setIsLoading(false);
@@ -46,31 +46,39 @@ export default function Routes() {
   }
 
   // If a user is logged in load the auth routing
-  if (user){
-      return(
-        <NavigationContainer>
-          <Stack.Navigator
-            screenOptions={{
-              headerTitleStyle: {
-                fontWeight: 'bold',
-                fontSize: 32,
-              },
-              headerTitleAlign: "left",
-            }}
-          >
-              <Stack.Screen name="Reminders" component={ TabNavigation } />
-              <Stack.Screen name="Create Reminder" component={AddEventScreen} options={{headerLeft: () => null}}/>
-          </Stack.Navigator>
-        </NavigationContainer>
-      );
-  } else { // Return the pre auth routing
+  if (user) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerTitleStyle: {
+              fontWeight: "bold",
+              fontSize: 32,
+            },
+            headerTitleAlign: "left",
+          }}
+        >
+          <Stack.Screen name="Reminders" component={TabNavigation} />
+          <Stack.Screen
+            name="Create Reminder"
+            component={AddEventScreen}
+            options={{ headerLeft: () => null }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    // Return the pre auth routing
     return (
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Welcome" headerMode="none">
           <Stack.Screen name="Welcome" component={WelcomeScreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Register" component={RegisterScreen} />
-          <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+          <Stack.Screen
+            name="ForgotPassword"
+            component={ForgotPasswordScreen}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     );
@@ -81,23 +89,16 @@ export default function Routes() {
 const TabNavigation = () => {
   return (
     <Tab.Navigator
-    tabBarOptions={{
-      labelStyle: {
-        fontSize: 16,
-        fontWeight: "bold",
-        position: 'absolute',
-      },
-          //activeTintColor: 'tomato',
-          // inactiveTintColor: 'grey',
-          // inactiveBackgroundColor: 'grey',
-          // activeBackgroundColor: 'grey',
-        }}>
-
-
-
+      tabBarOptions={{
+        labelStyle: {
+          fontSize: 20,
+          fontWeight: "bold",
+          position: "absolute",
+        },
+      }}
+    >
       <Tab.Screen name="Active" component={HomeScreen} />
-      <Tab.Screen name="Completed" component={completedRemindersScreen}/>
+      <Tab.Screen name="Completed" component={completedRemindersScreen} />
     </Tab.Navigator>
-  )
-}
-
+  );
+};
