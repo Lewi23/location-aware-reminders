52f9a4ad14e6957a39a942145aee45f15646a914
Lewis <Lewiswilson1497@gmail.com>
2021-03-07 23:12:35 +0000

    Updated list item to now handle completed reminders

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,41 +1,56 @@
-import React from 'react';
-import { StyleSheet, TouchableOpacity, Text } from 'react-native';
+import React from "react";
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
+import colors from "../utils/colors";
 
-
-export default function ListItem({ item, onPress, style }){
+export default function ListItem({ item, onPress, style }) {
   let title;
 
-  switch(item.location){
-    case 'Food, Drink and Multi Item Retail':
-      title = '🛒  '
+  switch (item.location) {
+    case "Food, Drink and Multi Item Retail":
+      title = "🛒  ";
       break;
-    case 'Nature':
-      title = '🌳  '
+    case "Nature":
+      title = "🌳  ";
       break;
   }
 
-  return(
-    <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
-    <Text>
-      <Text style={styles.item_title}>{title}</Text>
-      <Text>{item.reminder}</Text>
-    </Text>
-    </TouchableOpacity>
-  );
+  if (!item.completed) {
+    return (
+      <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
+        <Text>
+          <Text style={styles.item_title}>{title}</Text>
+          <Text style={styles.item_text}>{item.reminder}</Text>
+        </Text>
+      </TouchableOpacity>
+    );
+  } else {
+    return (
+      <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
+        <Text>
+          <Text style={styles.item_title}>{title}</Text>
+          <Text style={styles.item_text_completed}>{item.reminder}</Text>
+        </Text>
+      </TouchableOpacity>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   item_title: {
-    fontSize:40,
+    fontSize: 30,
+  },
+  item_text: {
+    fontSize: 20,
+  },
+  item_text_completed: {
+    fontSize: 20,
+    color: colors.white,
   },
   item: {
-    backgroundColor: 'black',
+    backgroundColor: "black",
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
-    fontSize: 20,
-    color: 'white',
-    borderRadius:10,
-    borderColor: "blue"
+    borderRadius: 10,
   },
 });
